d6b4d302e003cd4be48946c76e055c34a518c5de
cwout <cwout.coenen@gmail.com>
2015-09-18 14:27:15 +0200

    multi ical file support

--- generator.js
+++ generator.js
@@ -1,41 +1,109 @@
+var amount = 1;
+
 function generate() {
+	//clear output field
 	document.getElementById('out').value = "";
-	str = "http://thefaceless.be/icalQuery/ical.ics?ical_link="
-	url = encodeURIComponent(document.getElementById('ical_url').value).replace(/\./g,'%2E');
-	if (typeof(url) == 'undefined' || url == null || url == '') {
-		return false;
+	//basic link
+	str = "http://thefaceless.be/icalQuery/ical.ics?";
+	//parse the first one
+	el = [];
+	el['url'] = encodeURIComponent(document.getElementById('ical_url').value).replace(/\./g,'%2E');
+	el['hasUrl'] = !(typeof(el['url']) == 'undefined' || el['url'] == null || el['url'] == '');
+	el['regex'] = encodeURIComponent(document.getElementById('regex').value).replace(/\./g, '%2E');
+	el['r'] = true;
+	el['regex_not'] = encodeURIComponent(document.getElementById('regex_not').value).replace(/\./g, '%2E');
+	el['rn'] = true;
+	if (typeof(el['regex']) == 'undefined' || el['regex'] == null || el['regex'] == '') {
+		el['r'] = false;
 	}
-	str += url;
-	regex = encodeURIComponent(document.getElementById('regex').value).replace(/\./g, '%2E');
-	r = true;
-	regex_not = encodeURIComponent(document.getElementById('regex_not').value).replace(/\./g, '%2E');
-	rn = true;
-	if (typeof(regex) == 'undefined' || regex == null || regex == '') {
-		regex = '';
-		r = false;
+	if (typeof(el['regex_not']) == 'undefined' || el['regex_not'] == null || el['regex_not'] == '') {
+		el['rn'] = false;
 	}
-	if (typeof(regex) == 'undefined' || regex == null || regex_not == '') {
-		regex_not = '';
-		rn = false;
-	}
-	if (r) {
-		str += '&regex=' + regex;
-		if (document.getElementById('search_summary').checked) {
-			str += '&summary';
+	el['summary'] = document.getElementById('search_summary').checked;
+	el['description'] = document.getElementById('search_description').checked;
+	el['summary_not'] = document.getElementById('search_summary_not').checked;
+	el['description_not'] = document.getElementById('search_description_not').checked;
+	//create the array and push the first one onto the array
+	data = [];
+	data.push(el);
+
+	//repeat for other files
+	for (i = 2; i <= amount; i++) {
+		el = new Array();
+		el['url'] = encodeURIComponent(document.getElementById('ical_url'+i).value).replace(/\./g,'%2E');
+		el['hasUrl'] = !(typeof(el['url']) == 'undefined' || el['url'] == null || el['url'] == '');
+		el['regex'] = encodeURIComponent(document.getElementById('regex'+i).value).replace(/\./g, '%2E');
+		el['r'] = true;
+		el['regex_not'] = encodeURIComponent(document.getElementById('regex_not'+i).value).replace(/\./g, '%2E');
+		el['rn'] = true;
+		if (typeof(el['regex']) == 'undefined' || el['regex'] == null || el['regex'] == '') {
+			el['r'] = false;
 		}
-		if (document.getElementById('search_description').checked) {
-			str += '&description';
+		if (typeof(el['regex_not']) == 'undefined' || el['regex_not'] == null || el['regex_not'] == '') {
+			el['rn'] = false;
+		}
+		el['summary'] = document.getElementById('search_summary'+i).checked;
+		el['description'] = document.getElementById('search_description'+i).checked;
+		el['summary_not'] = document.getElementById('search_summary_not'+i).checked;
+		el['description_not'] = document.getElementById('search_description_not'+i).checked;
+		if (el['hasUrl'] == true)
+			data.push(el);
+	}
+	console.log(data.length);
+	for (i = 0; i < data.length; i++) {
+		if (i > 0)
+			str += '&';
+		str += 'ical_link' + (i==0?'':(i+1)) + '=';
+		str += data[i]['url'];
+		if (data[i]['r']) {
+			str += '&regex' + (i==0?'':(i+1)) + '=' + data[i]['regex'];
+			if (data[i]['summary']) {
+				str += '&summary' + (i==0?'':(i+1));
+			}
+			if (data[i]['description']) {
+				str += '&description' + (i==0?'':(i+1));
+			}
+		}
+		if (data[i]['rn']) {
+			str += '&regex_not' + (i==0?'':(i+1)) + '=' + data[i]['regex_not'];
+			if (data[i]['summary_not']) {
+				str += '&summary_not' + (i==0?'':(i+1));
+			}
+			if (data[i]['description_not']) {
+				str += '&description_not' + (i==0?'':(i+1));
+			}
 		}
 	}
-	if (rn) {
-		str += '&regex_not=' + regex_not;
-		if (document.getElementById('search_summary_not').checked) {
-			str += '&summary_not';
-		}
-		if (document.getElementById('search_description_not').checked) {
-			str += '&description_not';
-		}
-	}
+
 	document.getElementById('out').value = str;
-    return false;
+	return false;
+}
+
+function addForm(index) {
+	newText = '<hr><br />';
+	newText += '<label for="ical_url' + index + '">Original .ics file url: </label>';
+	newText += '<input type="text" name="ical_url' + index + '" id="ical_url' + index + '" class="half_width">';
+	newText += '<br /><br />';
+	newText += '<label for="regex' + index + '">Regular expression: </label>';
+	newText += '<input type="text" name="regex' + index + '" id="regex' + index + '" class="half_width">';
+	newText += '<br />';
+	newText += '<label for="search_summary' + index + '">Search in summary: </label>';
+	newText += '<input type="checkbox" name="search_summary' + index + '" id="search_summary' + index + '" checked="">';
+	newText += '<br />';
+	newText += '<label for="search_description' + index + '">Search in description: </label>';
+	newText += '<input type="checkbox" name="search_description' + index + '" id="search_description' + index + '" checked="">';
+	newText += '<br /><br />';
+	newText += '<label for="regex_not' + index + '">Regular expression (NOT): </label>';
+	newText += '<input type="text" name="regex_not' + index + '" id="regex_not' + index + '" class="half_width">';
+	newText += '<br />';
+	newText += '<label for="search_summary_not' + index + '">Search in summary: </label>';
+	newText += '<input type="checkbox" name="search_summary_not' + index + '" id="search_summary_not' + index + '">';
+	newText += '<br />';
+	newText += '<label for="search_description_not' + index + '">Search in description: </label>';
+	newText += '<input type="checkbox" name="search_description_not' + index + '" id="search_description_not' + index + '">';
+	newText += '<br /><br />';
+	newText += '<a id="new_form_link" href="#" onclick="return addForm(' + (index+1) + ');">Add another .ics file</a>';
+	link = document.getElementById('new_form_link').outerHTML = newText;
+	amount = index;
+	return false;
 }
--- generator.php
+++ generator.php
@@ -6,11 +6,11 @@
 	<body>
 		<form action="#" id="f">
 			<label for="ical_url">Original .ics file url: </label>
-			<input type="text" name="ical_url" id="ical_url">
+			<input type="text" name="ical_url" id="ical_url" class="half_width">
 			<br />
 			<br />
 			<label for="regex">Regular expression: </label>
-			<input type="text" name="regex" id="regex">
+			<input type="text" name="regex" id="regex" class="half_width">
 			<br />
 			<label for="search_summary">Search in summary: </label>
 			<input type="checkbox" name="search_summary" id="search_summary" checked="">
@@ -19,14 +19,17 @@
 			<input type="checkbox" name="search_description" id="search_description" checked="">
 			<br />
 			<br />
-			<label for="regex">Regular expression (NOT): </label>
-			<input type="text" name="regex_not" id="regex_not">
+			<label for="regex_not">Regular expression (NOT): </label>
+			<input type="text" name="regex_not" id="regex_not" class="half_width">
 			<br />
-			<label for="search_summary">Search in summary: </label>
+			<label for="search_summary_not">Search in summary: </label>
 			<input type="checkbox" name="search_summary_not" id="search_summary_not">
 			<br />
-			<label for="search_description">Search in description: </label>
+			<label for="search_description_not">Search in description: </label>
 			<input type="checkbox" name="search_description_not" id="search_description_not">
+			<br />
+			<br />
+			<a id="new_form_link" href="#" onclick="return addForm(2);">Add another .ics file</a>
 			<br />
 			<br />
 			<input type="submit" value="Generate Link" onclick="return generate();">
